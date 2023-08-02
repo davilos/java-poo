0dd491f498cb67908afec2bf4429b865cdf84926
davilos <daviloscostagg@hotmail.com>
2023-08-02 15:36:09 -0300

    adds imports, creates objects, and sets the locale

--- src/main/java/POO/application/ProgramStudent.java
+++ src/main/java/POO/application/ProgramStudent.java
@@ -1,4 +1,17 @@
 package POO.application;
 
+import POO.entities.Student;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ProgramStudent {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+        Student student = new Student();
+    }
 }
