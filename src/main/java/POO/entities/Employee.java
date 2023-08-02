a08833e0868b1c5ed3eeb1f786b6066afd620c3a
davilos <daviloscostagg@hotmail.com>
2023-08-02 14:48:52 -0300

    adds the methods to the class

--- src/main/java/POO/entities/Employee.java
+++ src/main/java/POO/entities/Employee.java
@@ -7,10 +7,14 @@ public class Employee {
     public double tax;
 
     public double netSalary() {
-
+        return grossSalary - tax;
     }
 
     public void increaseSalary(double percentage) {
+        grossSalary += grossSalary * percentage / 100.0;
+    }
 
+    public String toString() {
+        return "Employee: " + name + ", $ " + String.format("%.2f", netSalary());
     }
 }
