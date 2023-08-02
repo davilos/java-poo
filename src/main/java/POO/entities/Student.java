37799ffaf3bd9c7d7a9fb9ddda682d839832a1c7
davilos <daviloscostagg@hotmail.com>
2023-08-02 15:34:27 -0300

    adds attributes and the getFinalGrade method

--- src/main/java/POO/entities/Student.java
+++ src/main/java/POO/entities/Student.java
@@ -1,4 +1,20 @@
 package POO.entities;
 
 public class Student {
+
+    public double firstGrade;
+    public double secondGrade;
+    public double thirdGrade;
+
+    public double finalGrade;
+
+    public String getFinalGrade() {
+        finalGrade = firstGrade + secondGrade + thirdGrade;
+
+        if (finalGrade >= 60.00) {
+            return "FINAL GRADE = " + finalGrade + "\nPASS";
+        } else {
+            return "FINAL GRADE = " + finalGrade + "\nFAILED" + "\nMISSING" + (60.00 - finalGrade) + "POINTS";
+        }
+    }
 }
