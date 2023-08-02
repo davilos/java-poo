aac5e786388baf86411ef22217e26c702b77c798
davilos <daviloscostagg@hotmail.com>
2023-08-01 22:38:43 -0300

    creates the attributes and methods

--- src/main/java/POO/entities/Employee.java
+++ src/main/java/POO/entities/Employee.java
@@ -1,4 +1,16 @@
 package POO.entities;
 
 public class Employee {
+
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public double netSalary() {
+
+    }
+
+    public void increaseSalary(double percentage) {
+
+    }
 }
