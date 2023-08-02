7ee67035ee79e4b60ce593868e49654ea25ec314
davilos <daviloscostagg@hotmail.com>
2023-08-02 15:10:40 -0300

    adds a Employee object and prints the data

--- src/main/java/POO/application/ProgramEmployee.java
+++ src/main/java/POO/application/ProgramEmployee.java
@@ -1,8 +1,34 @@
 package POO.application;
 
+import POO.entities.Employee;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ProgramEmployee {
 
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+        Employee employee = new Employee();
+
+        System.out.print("Name: ");
+        employee.name = scanner.nextLine();
+
+        System.out.print("Gross salary: ");
+        employee.grossSalary = scanner.nextDouble();
+
+        System.out.print("Tax: ");
+        employee.tax = scanner.nextDouble();
+
+        System.out.printf("%n%s%n", employee);
+
+        System.out.print("\nWhich percentage to increase salary? ");
+        employee.increaseSalary(scanner.nextDouble());
+
+        System.out.printf("%nUpdated data: %s", employee);
 
     }
 }
