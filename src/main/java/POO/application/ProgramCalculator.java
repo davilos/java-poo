3f1c5f28a55556cb96cf0f49e02b6bc252642d8e
davilos <daviloscostagg@hotmail.com>
2023-08-03 13:28:09 -0300

    creates a Calculator object and uses its methods and attributes

---
+++ src/main/java/POO/application/ProgramCalculator.java
@@ -0,0 +1,28 @@
+package POO.application;
+
+import POO.util.Calculator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramCalculator {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        System.out.print("Enter radius: ");
+        double radius = scanner.nextDouble();
+
+        double c = calculator.circumference(radius);
+
+        double v = calculator.volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("PI value: %.2f%n", calculator.PI);
+    }
+}
