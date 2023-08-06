dfada27507ec4110cd4089f2178b018a8c3407fa
davilos <daviloscostagg@hotmail.com>
2023-08-06 14:02:44 -0300

    adds constructors to the Product class

--- src/main/java/POO/entities/Product.java
+++ src/main/java/POO/entities/Product.java
@@ -6,6 +6,20 @@ public class Product {
     private double price;
     private int quantity;
 
+    public Product() {
+    }
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
     public String getName() {
         return name;
     }
