17081603295edd8d155b9d91715907706c4a15bd
davilos <daviloscostagg@hotmail.com>
2023-08-08 00:37:33 -0300

    creates the bank system

---
+++ src/main/java/POO/entities/Bank.java
@@ -0,0 +1,56 @@
+package POO.entities;
+
+public class Bank {
+
+    private int accountNumber;
+    public String accountName;
+    double deposit;
+    public static final double tax = 5.00;
+
+    public Bank() {
+    }
+
+    public Bank(int accountNumber, String accountName) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+    }
+
+    public Bank(int accountNumber, String accountName, double initialDeposit) {
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.deposit = initialDeposit;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public double getDeposit() {
+        return deposit;
+    }
+
+    public void depositValue(double value) {
+        deposit += value;
+    }
+
+    public void withdrawValue(double value) {
+        if (value <= deposit) {
+            deposit = deposit - value - tax;
+        } else {
+            System.out.println("You don't have all that amount in your account!");
+        }
+    }
+
+    public String toString() {
+        return "Account " + getAccountNumber() + ", Holder: " + getAccountName() + ", Balance: $ " +
+                String.format("%.2f", getDeposit());
+    }
+}
